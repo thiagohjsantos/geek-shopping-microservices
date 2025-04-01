69d20dfc2ca5d1d609f14905038128d22e25badf
Thiago Santos <61206196+DevHenr@users.noreply.github.com>
2025-03-31 21:44:23 -0300

    feat: adding initial structure for CartAPI microservice

---
+++ GeekShopping.CartAPI/Repository/CartRepository.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using GeekShopping.CartAPI.Data.ValueObjects;
+using GeekShopping.CartAPI.Model.Context;
+
+namespace GeekShopping.CartAPI.Repository
+{
+    public class CartRepository : ICartRepository
+    {
+        private readonly Context context;
+        private IMapper _mapper;
+
+        public CartRepository(Context context, IMapper mapper)
+        {
+            this.context = context;
+            _mapper = mapper;
+        }
+
+        public Task<bool> ApplyCoupon(string userId, string couponCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ClearCart(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartVO> FindCartByUserId(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveCoupon(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveFromCart(long cartDetailsId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartVO> SaveOrUpdateCart(CartVO cart)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
