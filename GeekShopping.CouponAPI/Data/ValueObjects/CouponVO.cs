5550f5c4792f1b2438c56b103d39caa2f51a9e92
Thiago Santos <61206196+DevHenr@users.noreply.github.com>
2025-04-03 22:07:40 -0300

    feat: implementing CouponAPI microservice

---
+++ GeekShopping.CouponAPI/Data/ValueObjects/CouponVO.cs
@@ -0,0 +1,9 @@
+﻿namespace GeekShopping.CouponAPI.Data.ValueObjects
+{
+    public class CouponVO
+    {
+        public long Id { get; set; }
+        public string CouponCode { get; set; } = string.Empty;
+        public decimal DiscountAmount { get; set; }
+    }
+}
