4567bc67e5633971b9d72f809a975159959db13a
Thiago Santos <61206196+DevHenr@users.noreply.github.com>
2025-04-08 22:16:53 -0300

    feat: implement initial structure for OrderAPI microservice

---
+++ GeekShopping.OrderAPI/Model/Context/Context.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.OrderAPI.Model.Context
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options) {}
+
+        public DbSet<OrderDetail> Details { get; set; }
+        public DbSet<OrderHeader> Headers { get; set; }
+    }
+}
