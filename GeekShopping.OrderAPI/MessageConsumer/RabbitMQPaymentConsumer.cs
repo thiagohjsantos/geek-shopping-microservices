8fc473c7874765a83233966e3c9aba1b702eee95
Thiago Santos <61206196+DevHenr@users.noreply.github.com>
2025-04-13 17:47:18 -0300

    feat: add fanout exchange for payment result

--- GeekShopping.OrderAPI/MessageConsumer/RabbitMQPaymentConsumer.cs
+++ GeekShopping.OrderAPI/MessageConsumer/RabbitMQPaymentConsumer.cs
@@ -15,6 +15,8 @@ namespace GeekShopping.OrderAPI.MessageConsumer
         private readonly OrderRepository _repository;
         private IConnection _connection;
         private IModel _channel;
+        private const string ExchangeName = "FanoutPaymentUpdateExchange";
+        string queueName = "";
 
         public RabbitMQPaymentConsumer(OrderRepository repository)
         {
@@ -30,7 +32,9 @@ namespace GeekShopping.OrderAPI.MessageConsumer
             _connection = factory.CreateConnection();
             _channel = _connection.CreateModel();
 
-            _channel.QueueDeclare(queue: "orderpaymentresultqueue", false, false, false, arguments: null);
+            _channel.ExchangeDeclare(ExchangeName, ExchangeType.Fanout);
+            queueName = _channel.QueueDeclare().QueueName;
+            _channel.QueueBind(queueName, ExchangeName, "");
         }
 
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
@@ -47,7 +51,7 @@ namespace GeekShopping.OrderAPI.MessageConsumer
                 _channel.BasicAck(evt.DeliveryTag, false);   
             };
 
-            _channel.BasicConsume("orderpaymentresultqueue", false, consumer);
+            _channel.BasicConsume(queueName, false, consumer);
             return Task.CompletedTask;
         }
 
